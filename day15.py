cac82107cbb2d142b004023ad616a816887a4498
Karan174Pareek <143249558+Karan174Pareek@users.noreply.github.com>
2025-01-07 19:06:44 +0530

    Add files via upload

---
+++ day15.py
@@ -0,0 +1,30 @@
+# Graph: BFS Implementation
+from collections import deque
+
+def bfs(graph, start_node):
+    visited = set()
+    queue = deque([start_node])
+    bfs_order = []
+
+    while queue:
+        current = queue.popleft()
+
+        if current not in visited:
+            bfs_order.append(current)
+            visited.add(current)
+
+            for neighbor in graph[current]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+    return bfs_order
+
+graph = {
+    0: [1, 2],
+    1: [0, 3, 4],
+    2: [0],
+    3: [1],
+    4: [1]
+}
+
+print("BFS Traversal:", bfs(graph, 0))
